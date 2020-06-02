f50166b2d2d61408e486e42012a26bdd32901833
Alexandre Paloschi Horta <alexandre@buey.net.br>
2020-06-01 23:40:25 -0300

    Initial commit.

---
+++ hangingman/game.py
@@ -0,0 +1,46 @@
+import random
+
+
+WORDS = ["dog", "table", "fish", "books", "vertigo"]
+
+
+class HangingmanEngine:
+    
+    def guess_word(self):
+        chances = 3
+        word = [x for x in self.select_word()]
+        player_matches = [None for char in word]
+        player_guesses = set()
+        
+        while 1:
+            print(word, player_matches, sorted(list(player_guesses)), chances)
+            guess = self.take_a_guess()
+            if guess in word:
+                matches = [i for i, _ in enumerate(word) if _ == guess]
+                for m in matches:
+                    player_matches[m] = guess
+            else:
+                player_guesses.add(guess)
+                chances -= 1
+            if chances == 0: return self.game_over()
+            if player_matches == word: return self.game_won()
+    
+    def select_word(self):
+        return random.choice(WORDS)
+    
+    def take_a_guess(self):
+        return input("> ")
+    
+    def game_over(self):
+        print("Game over")
+    
+    def game_won(self):
+        print("Game won")
+    
+    def play_game(self):
+        self.guess_word()
+
+
+if __name__ == "__main__":
+    game = HangingmanEngine()
+    game.play_game()
---
+++ tests/test_hangingman.py
@@ -0,0 +1,11 @@
+from hangingman import __version__, game
+
+
+def test_version():
+    assert __version__ == '0.1.0'
+
+def test_select_word():
+    pass
+
+
+
