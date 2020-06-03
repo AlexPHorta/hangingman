4a91fe769e7e82cf9e1c61f574a0c23bf9b8f885
AlexPHorta <alexandre@buey.net.br>
2020-06-03 17:44:20 -0300

    Add word files.

--- hangingman/game.py
+++ hangingman/game.py
@@ -6,13 +6,13 @@ WORDS = ["dog", "table", "fish", "books", "vertigo"]
 
 
 class HangingmanEngine:
-    
+
     def guess_word(self):
         chances = 3
         word = [x.upper() for x in self.select_word()]
         player_matches = [None for char in word]
         player_guesses = set()
-        
+
         while 1:
             print(word, player_matches, sorted(list(player_guesses)), chances)
             guess = self.take_a_guess()
@@ -23,24 +23,26 @@ class HangingmanEngine:
             else:
                 player_guesses.add(guess)
                 chances -= 1
-            if chances == 0: return self.game_over()
-            if player_matches == word: return self.game_won()
-    
+            if chances == 0:
+                return self.game_over()
+            if player_matches == word:
+                return self.game_won()
+
     def select_word(self):
         return random.choice(WORDS)
-    
+
     def take_a_guess(self):
         letter = ""
         while letter not in string.ascii_letters or len(letter) != 1:
             letter = input("> ")
         return letter.upper()
-    
+
     def game_over(self):
         print("Game over")
-    
+
     def game_won(self):
         print("Game won")
-    
+
     def play_game(self):
         self.guess_word()
 
